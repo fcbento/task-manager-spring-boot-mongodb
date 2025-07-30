a8acb639e4ef33b3a9c947afc43459452855257a
fcbento <felipe.16costa@gmail.com>
2025-07-30 17:13:30 -0300

    feat: create exceptions for email and password handling

---
+++ src/main/java/org/task/service/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package org.task.service.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException{
+    public EmailAlreadyExistsException(String message){
+        super(message);
+    }
+}
